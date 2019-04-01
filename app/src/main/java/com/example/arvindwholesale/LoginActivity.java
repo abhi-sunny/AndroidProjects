4026aa724493f6e9eaea28a7cd9ee52e8019cb16
abhi_sunny <abhi.sunny.chauhan@gmail.com>
2019-04-01 15:34:15 +0530

    Changed progress bar at Login screen

--- app/src/main/java/com/example/arvindwholesale/LoginActivity.java
+++ app/src/main/java/com/example/arvindwholesale/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -25,6 +27,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
         UserName = findViewById(R.id.EditText_UserName);
